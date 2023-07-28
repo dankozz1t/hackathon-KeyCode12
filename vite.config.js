6bc9aa68d95e36c5acb44b324975ced3715648f5
Alex-Taylor Danko <AlexDankoXXL@gmail.com>
2023-07-28 14:03:58 +0300

    Initial commit

---
+++ vite.config.js
@@ -0,0 +1,29 @@
+import { defineConfig } from 'vite';
+import { fileURLToPath, URL } from "node:url";
+import glob from 'glob';
+import injectHTML from 'vite-plugin-html-inject';
+import FullReload from 'vite-plugin-full-reload';
+
+export default defineConfig({
+  root: 'src',
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `$injectedColor: orange;`,
+      },
+    },
+  },
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
+
+  build: {
+    rollupOptions: {
+      input: glob.sync('./src/*.html'),
+    },
+    outDir: '../dist',
+  },
+  plugins: [injectHTML(), FullReload(['./src/**/**.html'])],
+});
